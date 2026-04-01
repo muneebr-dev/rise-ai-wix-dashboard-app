7221a8db7ca8432208e6f6c6c03613bce598d387
Muneeb <muneebrdev@gmail.com>
2026-04-01 16:50:51 +0500

    Initial commit from Create Wix App

---
+++ src/extensions/dashboard/pages/my-page/my-page.extension.ts
@@ -0,0 +1,8 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: 'dd98a902-5e18-49f4-973b-f78e8eea40c5',
+  title: 'My Page',
+  routePath: '',
+  component: './extensions/dashboard/pages/my-page/my-page.tsx',
+});
