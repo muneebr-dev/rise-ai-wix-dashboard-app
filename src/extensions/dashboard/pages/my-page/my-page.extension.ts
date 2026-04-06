1f253470b69f2c1c09efe040bfec0da90518ff31
Muneeb <muneebrdev@gmail.com>
2026-04-06 13:20:49 +0500

    shift the accounts

--- src/extensions/dashboard/pages/my-page/my-page.extension.ts
+++ src/extensions/dashboard/pages/my-page/my-page.extension.ts
@@ -1,7 +1,7 @@
 import { extensions } from '@wix/astro/builders';
 
 export default extensions.dashboardPage({
-  id: 'dd98a902-5e18-49f4-973b-f78e8eea40c5',
+  id: 'd753bdce-42c3-46b6-8384-331b7824a359',
   title: 'Rise AI',
   routePath: '',
   component: './extensions/dashboard/pages/my-page/my-page.tsx',
